4be9180f5c68d2ae6034e93a152053dbb4169359
g g <gabedotgutierrez@gmail.com>
2024-10-21 14:30:23 -0400

    lecture end

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -9,6 +9,9 @@ const verifyToken =  require('../middleware/verify-token')
 router.get('/:userId', verifyToken, async (req, res) => { //adding middleware literally in the middle here. 
   try {
     const user = await User.findById(req.params.userId); // try to find a user!
+    if (req.user._id !== req.params.userId){ // check the ID of the user! ADDED LINES HERE
+        return res.status(401).json({ error: "Unauthorized"})
+    }
     if (!user) {
       res.status(404); //user not found is a 404
       throw new Error('Profile not found.'); // throw makes the error trip. or else this would just return null
